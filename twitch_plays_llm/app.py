e6b06e6de41ea094f7e62863e3db8b6e10abc312
Graylan <34530588+graylan0@users.noreply.github.com>
2023-07-18 19:00:50 -0400

    fix img generation

--- twitch_plays_llm/app.py
+++ twitch_plays_llm/app.py
@@ -2,19 +2,15 @@ import asyncio
 import openai
 from typing import List
 from fastapi import FastAPI
-from .llm_game import LlmGame
-from .llm_game import CharacterMemory
+from .llm_game import LlmGame, CharacterMemory
 from .llm_twitch_bot import LlmTwitchBot
 from .models import Proposal, StoryEntry
 from .story_generator import StoryGenerator
-
-app = FastAPI()
-
-# We need to maintain a reference to running coroutines to prevent GC
-background_task = None
-
 from fastapi.middleware.cors import CORSMiddleware
 
+app = FastAPI()
+background_task = None
+image_url = None
 
 origins = [
     "http://localhost:3000",  # React app is served from this URL
@@ -29,33 +25,9 @@ app.add_middleware(
 )
 
 @app.on_event('startup')
-def on_startup():
-    global background_task
-    app.state.game = game = LlmGame()
-    app.state.bot = bot = LlmTwitchBot(game)
-    game.hooks = bot
-    background_task = asyncio.create_task(bot.start())
+async def on_startup():
+    global background_task, image_url
 
-
-@app.get('/proposals')
-def get_proposals() -> List[Proposal]:
-    game: LlmGame = app.state.game
-    return game.proposals
-
-
-@app.get('/story-history')
-def get_story_history() -> List[StoryEntry]:
-    game: LlmGame = app.state.game
-    return game.generator.past_story_entries
-
-@app.get('/vote-time-remaining')
-def get_vote_time_remaining():
-    game: LlmGame = app.state.game
-    remaining_time = game.calculate_remaining_time()
-    return {"remaining_time": remaining_time}
-
-@app.post("/generate-image")
-async def generate_image():
     # Create a CharacterMemory and StoryGenerator instance
     character_memory = CharacterMemory()
     generator = StoryGenerator(character_memory)
@@ -71,5 +43,32 @@ async def generate_image():
         size="512x512"
     ))
 
+    # Store the generated image URL
+    image_url = response['data'][0]['url']
+
+    app.state.game = game = LlmGame()
+    app.state.bot = bot = LlmTwitchBot(game)
+    game.hooks = bot
+    background_task = asyncio.create_task(bot.start())
+
+@app.get('/proposals')
+def get_proposals() -> List[Proposal]:
+    game: LlmGame = app.state.game
+    return game.proposals
+
+@app.get('/story-history')
+def get_story_history() -> List[StoryEntry]:
+    game: LlmGame = app.state.game
+    return game.generator.past_story_entries
+
+@app.get('/vote-time-remaining')
+def get_vote_time_remaining():
+    game: LlmGame = app.state.game
+    remaining_time = game.calculate_remaining_time()
+    return {"remaining_time": remaining_time}
+
+@app.get("/generate-image")
+async def generate_image():
     # Return the generated image
-    return {"image": response['data'][0]['url']}
+    global image_url
+    return {"image": image_url}
