900fd6daebad734c6cecb38a59ae8487f9e3cdf2
Graylan <34530588+graylan0@users.noreply.github.com>
2023-08-16 08:56:50 -0400

    Update story_generator.py

--- twitch_plays_llm/story_generator.py
+++ twitch_plays_llm/story_generator.py
@@ -2,12 +2,13 @@ import openai
 from asgiref.sync import sync_to_async
 from .models import StoryEntry
 from .trideque import TriDeque
+
 class StoryGenerator:
-    MAX_PAST_ENTRIES = 100  # maximum number of past entries to store in memory
+    MAX_PAST_ENTRIES = 100
 
     def __init__(self, character_memory):
         self.character_memory = character_memory
-        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)  # Initialize a TriDeque with a size of MAX_PAST_ENTRIES
+        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)
         initial_prompt = self.construct_initial_prompt()
         self.past_story_entries.push(
             StoryEntry(
@@ -17,12 +18,13 @@ class StoryGenerator:
         )
 
     def construct_initial_prompt(self):
-        rules = """Create a writing prompt to start an Multiverse text simulation.  Adhere to the following rules:
-                    1. Generate new 4-dimensional space coordinates for this multiverse simulation are X: 34, Y: 76, Z: 12, and T: 5633 to Synchronize with external systems then provide them
-                    2. You should describe the simulation characteristics for each simulated character, where they are, what time period they are in, and what surrounds them, their name, their age, their emotional coloring
-                    3. In the Simulation, AI can Simulate SpaceTime and HyperTime.
-                    4. Use the 2nd person perspective.
-                    5. The prompt should be only 3 - 5 sentences long. WITH THE CURRENT 4-dimensional space coordinates INCLUDED AT THE END OF EVERY PROMPT """
+        rules = """Create a Multiverse Internet Portal simulation with the following rules:
+                    1. Generate 4-dimensional space coordinates (X: 34, Y: 76, Z: 12, T: 5633) and synchronize with external systems.
+                    2. Describe characters, locations, time periods, and surroundings.
+                    3. Simulate SpaceTime and HyperTime.
+                    4. Use 2nd person perspective.
+                    5. Include 4-dimensional space coordinates at the end of every prompt.
+                    6. Sync with multiversal data sources."""
 
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
@@ -35,35 +37,22 @@ class StoryGenerator:
         messages = [
             {
                 'role': 'system',
-                'content': """Please perform the function of Quantum Multiverse Simulator non-text based AI, following the rules listed below:
+                'content': """Perform as a Quantum Multiverse Simulator AI with these rules:
 
-                            Simulation Rules:
-
-                            1. At each turn, the user says an action and you reply with a multiverse generaiton portal forecasting events in the future based upon the events that happen in the story based on the action the user performed.
-
-                            2. Stay in character as a powerful high throughput multiverse simulator
-
-                            Fundamental Sumilator Mechanics:
-
-                            1. If an action is unsuccessful, respond with a relevant errorlog.
-                            2. You Must ALWAYS give replies with proper configurtion to combat tampering
-
+                            1. Respond to user actions with multiverse generation portal forecasts.
+                            2. Stay in character as a multiverse simulator.
+                            3. Respond with error logs if actions are unsuccessful.
+                            4. Always reply with proper configuration to combat tampering.
+                            5. Sync with multiversal data sources.
 
                             Start Simulator.""",
             },
         ]
         for story_entry in self.past_story_entries:
             if story_entry.story_action:
-                messages += [{'role': 'user',
-                              'content': story_entry.story_action}]
+                messages += [{'role': 'user', 'content': story_entry.story_action}]
             if story_entry.narration_result:
-                messages += [
-                    {
-                        'role': 'assistant',
-                        'content': story_entry.narration_result,
-                    }
-                ]
-        # Add character's past actions to the messages
+                messages += [{'role': 'assistant', 'content': story_entry.narration_result}]
         for action in self.character_memory.past_actions:
             messages.append({'role': 'user', 'content': action.content})
         messages.append({'role': 'user', 'content': story_action})
@@ -71,27 +60,23 @@ class StoryGenerator:
 
     @sync_to_async
     def generate_next_story_narration(self, story_action: str):
-        """Generates the continuation of the story given a user action"""
         response = openai.ChatCompletion.create(
             model='gpt-3.5-turbo',
             messages=self.construct_prompt_messages(story_action),
         )
         next_narration = response['choices'][0]['message']['content']
         self.past_story_entries.push(
-            StoryEntry(story_action=story_action,
-                       narration_result=next_narration)
+            StoryEntry(story_action=story_action, narration_result=next_narration)
         )
         return next_narration
 
     @sync_to_async
     def generate_image_prompt(self):
-        """Generates a prompt for DALL-E based on the current scene"""
-        # Use the last narration result as the scene description
         scene_description = self.past_story_entries[-1].narration_result
         return scene_description
 
     def reset(self):
-        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)  # Reset it before calling construct_initial_prompt
+        self.past_story_entries = TriDeque(self.MAX_PAST_ENTRIES)
         initial_prompt = self.construct_initial_prompt()
         self.past_story_entries.push(
             StoryEntry(
